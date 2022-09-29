5152d01d14c39998d5500cf0717bdca85c703012
Emre SAGLAM <69041945+cukulaci@users.noreply.github.com>
2022-09-29 08:45:58 +0300

    Add files via upload

---
+++ stringsDemo/src/stringsDemo/Main.java
@@ -0,0 +1,42 @@
+package stringsDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String mesaj = "          Bugün hava çok güzel          ";
+
+		System.out.println(mesaj);
+
+		/*  System.out.println("Eleman sayısı : " + mesaj.length());
+
+		System.out.println("5. eleman : " + mesaj.charAt(4));
+		System.out.println(mesaj.concat(" Yaşasın!"));
+		System.out.println(mesaj);
+
+		System.out.println(mesaj.startsWith("A"));
+
+		System.out.println(mesaj.endsWith("l"));
+
+		char[] karakterler = new char[5];
+		mesaj.getChars(0, 4, karakterler, 0);
+		System.out.println(karakterler);
+
+		System.out.println(mesaj.indexOf("av"));     */
+		
+		String yeniMesaj = mesaj.replace(' ', '-'); //oldChar olan boşluk yerine newChar olan - işaretini getirir.
+		System.out.println(yeniMesaj);
+		
+		System.out.println(mesaj.substring(2,4)); //2. indexten itibaren alır. 4'e kadar. 4 yazmazsak hepsini alır.
+		
+		for(String kelime: mesaj.split(" ")) {
+			System.out.println(kelime);
+		}
+		
+		System.out.println(mesaj.toLowerCase()); //bütün harfleri küçük yapar.
+		System.out.println(mesaj.toUpperCase()); //bütün harfleri büyük yapar.
+
+		System.out.println(mesaj.trim()); //başında ve sonundaki boşlukları siler
+		
+	}
+
+}
